627cc6ae8905a139faaf5622d0323d59589f8a7c
Kevin Matthes <kevin.matthes@s2019.tu-chemnitz.de>
2022-06-06 18:07:24 +0200

    Define main behaviour

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,82 @@
-fn main() {
-    println!("Hello, world!");
+/*********************** GNU General Public License 2.0 ***********************\
+|                                                                              |
+|  Copyright (C) 2022 Kevin Matthes                                            |
+|                                                                              |
+|  This program is free software; you can redistribute it and/or modify        |
+|  it under the terms of the GNU General Public License as published by        |
+|  the Free Software Foundation; either version 2 of the License, or           |
+|  (at your option) any later version.                                         |
+|                                                                              |
+|  This program is distributed in the hope that it will be useful,             |
+|  but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+|  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+|  GNU General Public License for more details.                                |
+|                                                                              |
+|  You should have received a copy of the GNU General Public License along     |
+|  with this program; if not, write to the Free Software Foundation, Inc.,     |
+|  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.                 |
+|                                                                              |
+\******************************************************************************/
+
+fn main() -> sysexits::ExitCode {
+    print!("Formatting the code ... ");
+
+    let format = match std::process::Command::new("cargo").arg("fmt").output() {
+        Ok(process) => process,
+        Err(error) => {
+            eprintln!("{error}");
+            return sysexits::ExitCode::Unavailable;
+        }
+    };
+
+    if format.status.code().unwrap() != 0 {
+        eprintln!("Failure:\n{:?}", String::from_utf8(format.stderr));
+        return sysexits::ExitCode::DataErr;
+    } else {
+        println!("Okay.")
+    }
+
+    print!("Checking whether the code compiles ... ");
+
+    let check = match std::process::Command::new("cargo").arg("check").output() {
+        Ok(process) => process,
+        Err(error) => {
+            eprintln!("{error}");
+            return sysexits::ExitCode::Unavailable;
+        }
+    };
+
+    if check.status.code().unwrap() != 0 {
+        eprintln!("Failure:\n{:?}", String::from_utf8(check.stderr));
+        return sysexits::ExitCode::DataErr;
+    } else {
+        println!("Okay.")
+    }
+
+    print!("Linting the code ... ");
+
+    let clippy = match std::process::Command::new("cargo")
+        .arg("clippy")
+        .arg("--fix")
+        .arg("--allow-dirty")
+        .arg("--allow-staged")
+        .output()
+    {
+        Ok(process) => process,
+        Err(error) => {
+            eprintln!("{error}");
+            return sysexits::ExitCode::Unavailable;
+        }
+    };
+
+    if clippy.status.code().unwrap() != 0 {
+        eprintln!("Failure:\n{:?}", String::from_utf8(clippy.stderr));
+        return sysexits::ExitCode::DataErr;
+    } else {
+        println!("Okay.");
+    }
+
+    sysexits::ExitCode::Ok
 }
+
+/******************************************************************************/
